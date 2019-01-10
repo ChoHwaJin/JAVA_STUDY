ae84098baa51398b39bbbaeaa217f5e400972e96
admin <admin@10.137.138.109>
2019-01-10 17:46:33 +0900

    Chapter04

---
+++ src/icehs/science/chapter04/CalculatorTest.java
@@ -0,0 +1,25 @@
+package icehs.science.chapter04;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 20;
+		int num2 = 7;
+		
+		int plusNum = num1 + num2;
+		int minusNum = num1 - num2;
+		int multipleNum = num1 * num2;
+		int divisionNum = num1 / num2;
+		int remainderNum = num1 % num2;
+		
+		System.out.println("µ¡¼À °á°ú: " + plusNum);
+		System.out.println("»¬¼À °á°ú: " + minusNum);
+		System.out.println("°ö¼À °á°ú: " + multipleNum);
+		System.out.println("³ª´°¼À °á°ú: " + divisionNum);
+		System.out.println("³ª¸ÓÁö: " + remainderNum);
+		
+
+	}
+
+}
---
+++ src/icehs/science/chapter04/InDeOperatorTest.java
@@ -0,0 +1,16 @@
+package icehs.science.chapter04;
+
+public class InDeOperatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 20;
+		
+		System.out.println(++num);
+		System.out.println(num++);
+		System.out.println(--num);
+		System.out.println(num--);
+
+	}
+
+}
